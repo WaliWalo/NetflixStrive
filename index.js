a7b2a7867623c6a2209b959cfccfbc766946a177
Ademuyiwa Otubusin <tay4real@gmail.com>
2020-10-16 11:13:20 +0100

    created movie object and genre array

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-//create movie class with constructors
+// Movie Object
+movies = [];
+
+movie = {
+  title: "",
+  genre: "",
+  date_released: "",
+  img_path: "",
+};
+
+genre = [
+  "Horror",
+  "Action",
+  "Drama",
+  "Science Fiction",
+  "Comedy",
+  "Crime Film",
+  "Romance",
+  "Western",
+  "Thriller",
+  "Adventure",
+  "Animation",
+  "War",
+  "Documentaries",
+  "Martial Arts",
+];
 
 const createMain = function () {};
